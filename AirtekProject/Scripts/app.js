033f3a2be69cf549a53bc2da9984149dc3cf5297
Pablo Seijas <seijasdev18@gmail.com>
2020-07-31 18:35:40 -0400

    Gráfico de dona y base de datos

--- AirtekProject/Scripts/app.js
+++ AirtekProject/Scripts/app.js
@@ -6,8 +6,10 @@
     sport = [];
     cast = [];
     eng = [];
+    
 
     $.get("Home/GetData", function (data, status) {
+
         
             for (i = 0; i <= data.length; i++) {
                 if (i === data.length) { break; }
@@ -18,7 +20,7 @@
                 eng.push(data[i].ingles);
             }
            
-
+      
 
         var ctx = document.getElementById('math').getContext('2d');
         var myChart = new Chart(ctx, {
@@ -58,8 +60,8 @@
             }
         });
 
-        var ctx = document.getElementById('sci').getContext('2d');
-        var myChart = new Chart(ctx, {
+        var ctx2 = document.getElementById('sci').getContext('2d');
+        var myChart = new Chart(ctx2, {
             type: 'bar',
             data: {
                 labels: ['Examen #1', 'Examen#2', 'Examen#3', 'Examen#4', 'Examen#5'],
@@ -96,8 +98,8 @@
             }
         });
 
-        var ctx = document.getElementById('sport').getContext('2d');
-        var myChart = new Chart(ctx, {
+        var ctx3 = document.getElementById('sport').getContext('2d');
+        var myChart = new Chart(ctx3, {
             type: 'bar',
             data: {
                 labels: ['Examen #1', 'Examen#2', 'Examen#3', 'Examen#4', 'Examen#5'],
@@ -134,8 +136,8 @@
             }
         });
 
-        var ctx = document.getElementById('cast').getContext('2d');
-        var myChart = new Chart(ctx, {
+        var ctx4 = document.getElementById('cast').getContext('2d');
+        var myChart = new Chart(ctx4, {
             type: 'bar',
             data: {
                 labels: ['Examen #1', 'Examen#2', 'Examen#3', 'Examen#4', 'Examen#5'],
@@ -172,8 +174,8 @@
             }
         });
 
-        var ctx = document.getElementById('eng').getContext('2d');
-        var myChart = new Chart(ctx, {
+        var ctx5 = document.getElementById('eng').getContext('2d');
+        var myChart = new Chart(ctx5, {
             type: 'bar',
             data: {
                 labels: ['Examen #1', 'Examen#2', 'Examen#3', 'Examen#4', 'Examen#5'],
@@ -209,8 +211,47 @@
                 }
             }
         });
+
+
+        var ctx6 = document.getElementById('doughnut').getContext('2d');
+        var myChart = new Chart(ctx6, {
+            type: 'doughnut',
+            data: {
+                labels: ['Examen #1', 'Examen#2', 'Examen#3', 'Examen#4', 'Examen#5'],
+                datasets: [{
+                    label: 'Inglés',
+                    data: eng,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
     });
-    
+       
 
  
     
