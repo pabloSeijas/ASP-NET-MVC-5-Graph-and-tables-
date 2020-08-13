5082482a319092a0fbbc0ec0a4ebf3e85ac2c6a7
Pablo Seijas <seijasdev18@gmail.com>
2020-08-13 08:16:46 -0400

    Final V1.0
    .

--- AirtekProject/Scripts/app.js
+++ AirtekProject/Scripts/app.js
@@ -1,5 +1,5 @@
 ﻿//GLOBALS
-var vpref, vsucursal, vtecnologia, vtotal, i, e;
+var vpref, vsucursal, vtecnologia, vtotal, i, e, g, s;
     vpref = [];
     vsucursal = [];
     vtotal = [];
@@ -9,13 +9,17 @@ function onlyUnique(value, index, self) {
     return self.indexOf(value) === index;
 }
 
+function myFunc(total, num) {
+    return total + num;
+}
+
 $(document).ready(function () {
 
     $.get("Home/equiposGetData", function (data, status) {
         var cliente;
 
-        var editor; // use a global for the submit and return data rendering in the examples
-        console.log(data)
+        var editor;
+        
         for (i = 0; i <= data.length; i++) {
             if (i === data.length) { break; }
             vpref.push(data[i].pref);
@@ -29,6 +33,66 @@ $(document).ready(function () {
         var tecnologia = vtecnologia.filter(onlyUnique);
         var dom = document.getElementById("container");
         var myChart = echarts.init(dom);
+    
+        //CLASIFICADOS POR TECNOLOGÍA
+        var CM = [];
+        var GP = [];
+        var WL = [];
+        //TOTALES
+        var totalCM = [];
+        var totalGP = [];
+        var totalWL = [];
+        
+        
+        for (e = 0; e < data.length; e++) {
+            //PARA TOTAL
+            if (data[e].tecnologia === 'cablemodem') {
+                CM.push(data[e]);
+            }
+            if (data[e].tecnologia === 'gpon') {
+                GP.push(data[e]);
+            }
+            if (data[e].tecnologia === 'wireless') {
+                WL.push(data[e]);
+            }
+            //PARA TECNOLOGÍA
+            if (data[e].tecnologia === 'cablemodem') {
+                totalCM.push(data[e].total);
+            }
+            if (data[e].tecnologia === 'gpon') {
+                totalGP.push(data[e].total);
+            }
+            if (data[e].tecnologia === 'wireless') {
+                totalWL.push(data[e].total);
+            }
+        }
+        //SUMATORIA TOTALES
+        var sumCM = totalCM.reduce(myFunc);
+        var sumGP = totalGP.reduce(myFunc);
+        var sumWL = totalWL.reduce(myFunc);
+    
+        var graphData = [];
+
+        for (g = 0; g < data.length; g++) {
+            graphData.push({
+                name: data[g].sucursal,
+                value: data[g].total,
+                itemStyle: {
+                    normal: {
+                        color: '#5f'+Math.floor(Math.random() * 10000),
+                        lineStyle: {
+                            color: 'white'
+                        },
+                        areaStyle: {
+                            color: '#5f2626'
+                        },
+                    }
+                },
+            })  
+           
+        }
+       
+        
         var app = {};
         option = null;
         option = {
@@ -36,11 +100,23 @@ $(document).ready(function () {
                 trigger: 'item',
                 formatter: '{a} <br/>{b}: {c} ({d}%)'
             },
-            legend: {
+            legend: [{
+                bottom: 10, width: '100%',
                 orient: 'horizontal',
-                bottom: 10,
-                data: tecnologia
+                height: '100',
+                selectedMode: 'multiple',
+                //pageButtonPosition:'end',
+                //formatter:'legend {name} ',
+                data: sucursal,
+
             },
+            {
+                orient: 'vertical',left: 10, width: '50%', height: 'auto',
+                data: ['CM', 'WL', 'GP'],
+
+            }
+            ],
+
             series: [
                 {
                     name: 'Tecnologías',
@@ -57,11 +133,53 @@ $(document).ready(function () {
                         show: false
                     },
                     data: [
-                        { value: 335, name: tecnologia[0] },
-                        { value: 679, name: tecnologia[1] },
-                        { value: 1548, name: tecnologia[2] },
-                        { value: 1548, name: tecnologia[3] }
-                    ]
+                        {
+                            //CM PARA CABLE MODEM
+                            value: sumCM, name: 'CM', itemStyle: {
+                                normal: {
+                                    color: '#502121',
+                                    lineStyle: {
+                                        color: 'white'
+                                    },
+                                    areaStyle: {
+                                        color: '#502121'
+                                    },
+                                },
+                            },
+                        },
+                        {
+                            //GP PARA GPON
+                            value: sumGP, name: 'GP', itemStyle: {
+                                normal: {
+                                    color: '#214d50',
+                                    lineStyle: {
+                                        color: 'white'
+                                    },
+                                    areaStyle: {
+                                        color: '#214d50'
+                                    },
+                                },
+                            },
+                        },
+                        {
+                            //WL PARA WIRELESS
+                            value: sumWL, name: 'WL', itemStyle: {
+                                normal: {
+                                    color: '#28671e',
+                                    lineStyle: {
+                                        color: 'white'
+                                    },
+                                    areaStyle: {
+                                        color: '#28671e'
+                                    },
+                                },
+                            },
+                        },
+                        {
+                           
+                        },
+
+                    ],
                 },
                 {
                     name: 'Sucursal',
@@ -82,7 +200,7 @@ $(document).ready(function () {
                          padding: [0, 7],
                         rich: {
                             a: {
-                                color: '#999',
+                                color: 'black',
                                 lineHeight: 15,
                                 align: 'center',
                                 width:-10
@@ -118,22 +236,12 @@ $(document).ready(function () {
                             }
                         }
                     },
-                    data: [
-                        { value: 335, name: sucursal[0] },
-                        { value: 310, name: sucursal[1] },
-                        { value: 234, name: sucursal[2] },
-                        { value: 135, name: sucursal[3] },
-                        { value: 104, name: sucursal[4] },
-                        { value: 251, name: sucursal[5] },
-                        { value: 147, name: sucursal[6] },
-                        { value: 102, name: sucursal[7] },
-                        { value: 102, name: sucursal[8] },
-                        { value: 102, name: sucursal[9] }
-                         
-                    ]
+                    data: graphData
+                       
                 }
             ]
         };;
+       
         if (option && typeof option === "object") {
             myChart.setOption(option, true);
         }
